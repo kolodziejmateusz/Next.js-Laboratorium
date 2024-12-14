39ec84fe35b91d6b3f9d0ddbdc19cba99a1aaba0
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-12-14 12:03:41 +0100

    firebase i .env

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,26 +1,23 @@
+require("dotenv").config();
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; // Add this import for Firebase Authentication
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1x7WnxeD9VrTnE0Ll8HnppnkpFUzV918",
-  authDomain: "frontend-app-next.firebaseapp.com",
-  projectId: "frontend-app-next",
-  storageBucket: "frontend-app-next.firebasestorage.app",
-  messagingSenderId: "264290008741",
-  appId: "1:264290008741:web:72c2601484e5e80fad574c",
-  measurementId: "G-NJHEKVK75K",
+  apiKey: process.env.API_KEY,
+  authDomain: "nextjs-laboratorium.firebaseapp.com",
+  projectId: "nextjs-laboratorium",
+  storageBucket: "nextjs-laboratorium.firebasestorage.app",
+  messagingSenderId: "824097786755",
+  appId: process.env.APP_ID,
+  measurementId: "G-V3JSSDYPXT",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app); // Initialize auth and export it
-
-export { auth };
