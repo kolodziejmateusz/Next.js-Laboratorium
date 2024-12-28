7a026e27e75325b807622a00db5dda0dc8307ca3
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-12-28 12:28:34 +0100

    wylogowywanie

--- app/(demo)/(protected)/user/logout/page.js
+++ app/(demo)/(protected)/user/logout/page.js
@@ -1,8 +1,19 @@
+"use client";
+import { signOut } from "firebase/auth";
+import { auth } from "@/app/lib/firebase";
+import { useRouter } from "next/navigation";
+
 function Logout() {
+  const router = useRouter();
+
+  const onSubmit = () => {
+    signOut(auth);
+    router.push("/user/login");
+  };
   return (
     <>
       <div className="h-4/5">
-        <form>
+        <form onSubmit={onSubmit}>
           <button type="submit" className="btn btn-accent w-full mt-3">
             Log Out
           </button>
